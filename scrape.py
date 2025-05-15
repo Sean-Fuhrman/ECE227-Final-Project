d2b38a1c9a9520d73fcac0c9d20d11144dfd2cc4
kendrick010 <nguyen.kendrick@yahoo.com>
2025-05-14 22:35:36 -0700

    batch to 1000 nodes

--- scrape.py
+++ scrape.py
@@ -7,8 +7,8 @@ input_file = "data/enwiki.wikilink_graph.2018-03-01.csv.gz"
 output_dir = "data/chunks/"
 os.makedirs(output_dir, exist_ok=True)
 
-start_chunk = 0
-chunksize = 100000
+chunk_size = 1000
+chunk_limit = 5
 
 def clean_chunk(chunk):
     return chunk.applymap(lambda x: x.strip() if isinstance(x, str) else x)
@@ -21,12 +21,12 @@ if __name__ == '__main__':
             header=None,
             names=["from_page_id", "from_page_title", "to_page_id", "to_page_title"],
             engine='python',
-            chunksize=chunksize,
+            chunksize=chunk_size,
             on_bad_lines='skip'
         )
 
         for chunk_num, chunk in enumerate(reader):
-            if chunk_num < start_chunk: continue  # Skip already processed chunks
+            if chunk_num > chunk_limit: break
     
             chunk = clean_chunk(chunk)
             G = nx.DiGraph()
